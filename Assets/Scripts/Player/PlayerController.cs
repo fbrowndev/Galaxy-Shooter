8e8ce568a939f21b0f33a6482afd03f84211f14f
Keke Brown <fbrowndev@gmail.com>
2023-01-02 15:08:53 -0500

    Boundaries added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     #region Player Variables
     [Header("Player Controls")]
     [SerializeField] float _speed = 5;
+
+    float _boundaryX = 10.5f;
     #endregion
 
     // Start is called before the first frame update
@@ -27,6 +29,9 @@ public class PlayerController : MonoBehaviour
     }
 
     #region Movement Methods
+    /// <summary>
+    /// Handling all movement for player sprite
+    /// </summary>
     void PlayerMovement()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
@@ -34,6 +39,32 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(horizontalMovement, verticalMovement, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        PlayerBounds();
+    }
+
+    /// <summary>
+    /// Player wrapping on x-axis and restricted on y-axis
+    /// </summary>
+    void PlayerBounds()
+    {
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        } 
+        else if(transform.position.y <= -3.45f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.45f, 0);
+        }
+
+        if(transform.position.x >= _boundaryX)
+        {
+            transform.position = new Vector3(-_boundaryX, transform.position.y, 0);
+        } 
+        else if(transform.position.x <= -_boundaryX)
+        {
+            transform.position = new Vector3(_boundaryX, transform.position.y, 0);
+        }
     }
     #endregion
 }
